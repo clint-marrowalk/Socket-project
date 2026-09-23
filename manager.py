72910ffa121130b084f477d6144848ddec547730
Clint_marrowalk <164465471+clint-marrowalk@users.noreply.github.com>
2026-09-23 21:36:23 +0000

    update

--- manager.py
+++ manager.py
@@ -1,12 +1,27 @@
 import socket   # the import is required for the project 
 
+peers= {} # stores all the info on each peer
 
 
 def register_peer(command): # give in the form of register ⟨peer-name⟩ ⟨IPv4-address⟩ ⟨m-port⟩ ⟨p-port⟩
     fields=command.split("|")
-     
+    peer_name = fields[1]
+    Ipv4 = fields[2]
+    mport = fields[3]
+    pport = fields [4]
+
+    if peer_name in peers:
+       return "FAILURE|names must be unique "
+    for people in peers.values():
+       used=(people[mport], people[pport])
+       if mport in used or pport in used:
+          return "FAILURE| ports must be unique"
+       
+
+    peers[peer_name]= {"ipv4":Ipv4, "m_port":mport, "p_port":pport, "state":"free"}
+
  
-    return  "a" 
+    return  "SUCCESS"
 
 
 
