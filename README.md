5ff45edeca462c99071788d105e94d2e95f93cf2
Clint_marrowalk <164465471+clint-marrowalk@users.noreply.github.com>
2026-09-23 21:19:48 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Socket-project
 
+
+commands are made with | to avoid breaking names like Dense Smoke
 ports 11000 to 11499
--- manager.py
+++ manager.py
@@ -2,5 +2,19 @@ import socket   # the import is required for the project
 
 
 
+def register_peer(command): # give in the form of register ⟨peer-name⟩ ⟨IPv4-address⟩ ⟨m-port⟩ ⟨p-port⟩
+    fields=command.split("|")
+     
+ 
+    return  "a" 
 
+
+
+
+
+def setup_dht(command):
+ fields=command.split("|")
+
+
+ return "SUCCESS now listing  n peers that together will construct the DHT. "
 
