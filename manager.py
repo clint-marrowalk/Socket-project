b92097019b1f60944be6aeb982eb2bc2c768d9ef
Clint_marrowalk <164465471+clint-marrowalk@users.noreply.github.com>
2026-09-25 17:14:18 +0000

    update

--- manager.py
+++ manager.py
@@ -1,9 +1,11 @@
 import socket   # the import is required for the project 
+import random 
 
 peers= {} # stores all the info on each peer
+members={} # stores members of the DHT
 
 
-def register_peer(command): # give in the form of register ⟨peer-name⟩ ⟨IPv4-address⟩ ⟨m-port⟩ ⟨p-port⟩
+def register_peer(command): # given in the form of register|(peer-name)(IPv4-address)|(m-port)|(p-port)
     fields=command.split("|")
     peer_name = fields[1] # grab the name 
     Ipv4 = fields[2]   # grab the ip addy
@@ -22,3 +24,19 @@ def register_peer(command): # give in the form of register ⟨peer-name⟩ ⟨IP
     peers[peer_name]= {"ipv4":Ipv4, "m_port":mport, "p_port":pport, "state":"Free"} 
     return  "SUCCESS"
 
+def setup_dht(command): # given in the form of setup-dht|(peer-name)|(n)|(YYYY)
+   fields=command.split("|") # splits the string to get the imporent value 
+   peer_name=fields[1]  # gets name
+   number=int(fields[2]) # gets N
+   year=int(fields[3]) #gets the year 
+   if (not(peer_name in peers)):      # all of these handle falure conditions
+      return "FAILURE|PEER NOT REGISTERED"
+   if number<3:
+      return "FAILURE|N MUST BE 3 OR GREATER"
+   if len(peers)<number:
+      return "FALURE|NOT ENOUGH MEMEBRS"
+   if members:    # empty dictionary are false 
+      return "FALURE|DHT SET UP BEFOREHAND"
+   members_to_add=randomint(1,n-1)
+   
+   
